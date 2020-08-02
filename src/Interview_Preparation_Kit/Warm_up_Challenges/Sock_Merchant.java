10f08610cbaa18485102450823b51ff8e1fa9e04
cmert2017 <cihanmert2075@gmail.com>
2020-08-02 00:04:05 -0400

    add Sock_Merchant2.java

---
+++ src/Interview_Preparation_Kit/Warm_up_Challenges/Sock_Merchant.java
@@ -0,0 +1,41 @@
+package Interview_Preparation_Kit.Warm_up_Challenges;
+/*
+https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ */
+public class Sock_Merchant {
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        String unique = "";
+        int result = 0;
+        for(int i=0;i<ar.length;i++){
+            int count = 0;
+            if(!unique.contains(""+ar[i])){
+                unique += ar[i] + " ";
+                for(int j = 0; j< ar.length;j++){
+                    if(ar[i]==ar[j]){
+                        count++;
+                    }
+                }
+            }
+
+            result+=count /2 ;
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 9;
+        int[] arr = {10,20,20,10,10,30,50,10,20};
+        System.out.println(sockMerchant(n,arr));
+
+        //test case 2:
+        int n2 = 10;
+        int[] arr2 = {1,1,3,1,2,1,3,3,3,3};
+        System.out.println(sockMerchant(n2,arr2));
+
+
+
+    }
+
+}
---
+++ src/Interview_Preparation_Kit/Warm_up_Challenges/Sock_Merchant2.java
@@ -0,0 +1,28 @@
+package Interview_Preparation_Kit.Warm_up_Challenges;
+
+public class Sock_Merchant2 {
+
+    static int sockMerchant(int n, int[] ar) {
+        int result = 0;
+
+
+
+
+
+
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int n = 9;
+        int[] arr = {10, 20, 20, 10, 10, 30, 50, 10, 20};
+        System.out.println(sockMerchant(n, arr));
+
+        //test case 2:
+        int n2 = 10;
+        int[] arr2 = {1, 1, 3, 1, 2, 1, 3, 3, 3, 3};
+        System.out.println(sockMerchant(n2, arr2));
+    }
+}
